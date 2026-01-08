9eae040684c1182bd3a1761812bdb5ff11d459c9
yosheDev <jacobjones7148@gmail.com>
2026-01-08 11:44:42 -0600

    Charge Chunk

---
+++ Assets/Battery/ChargeChunk.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ChargeChunk : MonoBehaviour
+{
+    [SerializeField] private byte chargeAmount = 50;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Battery>() != null)
+        {
+            Collider2D col = GetComponent<Collider2D>();
+            col.enabled = false;
+
+            collision.gameObject.GetComponent<Battery>().AddPercent(chargeAmount);
+            StartCoroutine(DestroyChunk());
+        }
+    }
+
+    private IEnumerator DestroyChunk()
+    {
+        yield return new WaitForSeconds(.1f);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Battery/ChargeParticleCollision.cs
+++ Assets/Battery/ChargeParticleCollision.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using System.Collections.Generic;
+using FunctionLibrary;
 
 public class ChargeParticleCollision : ParticleCollision
 {
-    public byte chargeAmount = 2;
+    public Vector2 particleSizeRange = new Vector2(.1f, .5f);
+    public Vector2 chargeAmountRange = new Vector2(1f, 5f);
     protected override void AssignTriggerColliders() /// Override this event in children to assign different colliders.
     {
         Collider2D playerCol = FindFirstObjectByType<BatteryController>().gameObject.GetComponent<Collider2D>();
         _particleSystem.trigger.AddCollider(playerCol);
     }
 
-    protected override void ParticleCollected(Component otherCollider)
+    protected override void ParticleCollected(Component otherCollider, float particleSize)
     {
         if (otherCollider.gameObject.GetComponent<Battery>() != null)
         {
-            otherCollider.gameObject.GetComponent<Battery>().AddPercent(chargeAmount);
+            otherCollider.gameObject.GetComponent<Battery>().AddPercent(FunctionLibraryF.MapRangeClamped(0.1f, 0.5f, 1f, 5f, particleSize));
         }
     }
 }
