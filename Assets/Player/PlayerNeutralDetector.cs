924528dde7cd442c51b085aabb131432dc8bb609
yosheDev <jacobjones7148@gmail.com>
2026-01-15 09:08:08 -0600

    Additional Neutral Detection Fixes
    - This time neutral detection occlusion check actually does what I expect.

--- Assets/Player/PlayerNeutralDetector.cs
+++ Assets/Player/PlayerNeutralDetector.cs
@@ -23,30 +23,53 @@ public class PlayerNeutralDetector : MonoBehaviour
             return;
         }
 
+        #region Occlusion Tracing
         // Trace to confirm there is not a magnet between found neutral actor and the player.
         RaycastHit2D[] hits = Physics2D.LinecastAll(batteryController.transform.position, collision.ClosestPoint(batteryController.transform.position));
-        Debug.DrawLine(batteryController.transform.position, collision.ClosestPoint(batteryController.transform.position), Color.blue, 1f);
-        //bool legitimate = false;
+        float nearestDistanceToOccluder = 99999f;
+        float distanceToNearestNeutral = Vector2.Distance(collision.ClosestPoint(batteryController.gameObject.transform.position), batteryController.gameObject.transform.position);
         for (int i = 0; i < hits.Length; i++)
         {
             /// If a component of the player, just ignore.
             if (hits[i].collider.gameObject.GetComponentInParent<BatteryController>() || hits[i].collider.gameObject.GetComponentInChildren<BatteryController>())
             {
-                //Debug.Log("Returned on 1. Obj was " + hits[i].collider.gameObject);
                 continue;
             }
 
-            if (!FunctionLibraryF.IsInLayerMask(hits[i].collider.gameObject, mask))
+            // Is this hit an occluder? If so, track distance.
+            if (LayerMask.LayerToName(hits[i].collider.gameObject.layer) == "MagnetSurface")
             {
-                Debug.Log("REturned on 2 Obj was " + hits[i].collider.gameObject);
-                return;
+                //Debug.Log("Occluder: " + hits[i].collider.gameObject);
+                float nearDistance = Vector2.Distance(hits[i].collider.ClosestPoint(batteryController.gameObject.transform.position), batteryController.gameObject.transform.position);
+                nearestDistanceToOccluder = nearDistance <= nearestDistanceToOccluder ? nearDistance : nearestDistanceToOccluder;
+                continue;
             }
+
+            // Is a closer neutral surface detected? If so, track distance. (This might be useless?)
+            //if (FunctionLibraryF.IsInLayerMask(hits[i].collider.gameObject, mask))
+            //{
+            //    float nearDistance = Vector2.Distance(hits[i].collider.ClosestPoint(batteryController.gameObject.transform.position), batteryController.gameObject.transform.position);
+            //    distanceToNearestNeutral = nearDistance <= distanceToNearestNeutral ? nearDistance : distanceToNearestNeutral;
+            //    continue;
+            //}
+            //else
+            //{
+            //    Debug.Log(hits[i].collider.gameObject + " was not in layerMask");
+            //}
+
         }
 
-        
+        // If occluded by something, stop execution.
+        //Debug.Log("Neutral near distance: " + distanceToNearestNeutral + " | Occluder distance: " + nearestDistanceToOccluder);
+        if (distanceToNearestNeutral >= nearestDistanceToOccluder)
+        {
+            return;
+        }
+        #endregion
+
         neutralOverlaps.Add(collision.gameObject);
         neutralDetected = true;
-        Debug.Log(collision.gameObject);
+        //Debug.Log("Neutral Added: " + collision.gameObject);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
