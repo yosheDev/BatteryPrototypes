1d2c3fb6d1eb7acaa6f7f2d77e6f91e0c547eee5
yosheDev <jacobjones7148@gmail.com>
2026-03-04 11:14:27 -0600

    Charge Wire Complete for now

--- Assets/Battery/ChargeParticleEmission.cs
+++ Assets/Battery/ChargeParticleEmission.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using FunctionLibrary;
+using UnityEngine.InputSystem.Utilities;
 
 public class ChargeParticleEmission : MonoBehaviour
 {
@@ -16,17 +18,27 @@ public class ChargeParticleEmission : MonoBehaviour
 
     [Header("Parameters")]
     [SerializeField] private float emissionRate = .5f;
+    [SerializeField] private float emissionRateVariance = .2f;
     [SerializeField] private float lifetime = 10f;
     [SerializeField] private EmitDirection emitDirection = EmitDirection.X;
-    [SerializeField] private float velocityAngleRadius = 10f;
-    [SerializeField] private Vector2 velocityRange = new Vector2(1f, 1.5f);
-    [SerializeField] private float angleVariance;
+    [SerializeField] private Vector2 velocityRange = new Vector2(2f, 4f);
+    [SerializeField] private float velocityAngleVariance = 20f;
+    [SerializeField] private HingeJoint2D swayHinge;
+    [SerializeField] private float swayIntensity = 1f;
+    private JointMotor2D swayMotor;
+
+    private bool randomBool = true;
     // Look into adding gravity as parameter for the particle system.
 
     private void Start()
     {
         _particleManager = GameObject.FindFirstObjectByType<ChargeParticleManager>();
+
+        swayMotor = new JointMotor2D();
+        UpdateMotor(1f * swayIntensity, 10000f);
+        
         StartCoroutine(EmitLoop());
+        StartCoroutine(SwayRandom());
     }
 
     private IEnumerator EmitLoop()
@@ -34,7 +46,7 @@ public class ChargeParticleEmission : MonoBehaviour
         while (true)
         {
             Emit();
-            yield return new WaitForSeconds(emissionRate);
+            yield return new WaitForSeconds(Mathf.Clamp(emissionRate + Random.Range(emissionRateVariance * -1f, emissionRateVariance), 0f, 30f));
         }
     }
 
@@ -66,8 +78,28 @@ public class ChargeParticleEmission : MonoBehaviour
                 vel = transform.right;
                 break;
         }
+        vel = Quaternion.Euler(0f, 0f, Random.Range(velocityAngleVariance * -1f, velocityAngleVariance)) * vel;
         emitParams.velocity = vel * Random.Range(velocityRange.x, velocityRange.y);
 
         _particleManager.EmitChargeParticle(emitParams, 1);
+    }
+
+    private IEnumerator SwayRandom()
+    {
+        while (true)
+        {
+            randomBool = !randomBool;
+            UpdateMotor((randomBool ? 1f : -1f) * swayIntensity * Random.Range(.8f, 1f), Random.Range(9000f, 10000f));
+            //yield return new WaitForSeconds(2f * Random.Range(.5f, 1.5f));
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void UpdateMotor(float motorSpeed, float maxTorque)
+    {
+        swayMotor.motorSpeed = motorSpeed * 100f;
+        swayMotor.maxMotorTorque = maxTorque;
+        swayHinge.useMotor = true;
+        swayHinge.motor = swayMotor;
     }
 }
