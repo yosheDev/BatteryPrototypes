177ed51f0bb614952a677f9a75ac36e9c2ac60dc
Jacob Jones <101999077+yosheDev@users.noreply.github.com>
2026-03-26 09:32:44 -0500

    Macrowave mostly complete

--- Assets/Scenes/Areas/Area 2/MechanicalAnimations/a2_r4_OvenRoom.cs
+++ Assets/Scenes/Areas/Area 2/MechanicalAnimations/a2_r4_OvenRoom.cs
@@ -62,24 +62,89 @@ public class a2_r4_OvenRoom : MonoBehaviour
 
         topIndicators.FlashIndicators(6f, .25f, .125f);
         yield return new WaitForSeconds(6f);
-        Debug.Log("Macrowave from the TOP");
+        //Debug.Log("Macrowave from the TOP");
         topRayHazards.EmitBegin();
         yield return new WaitForSeconds(5f);
         topRayHazards.EmitEnd();
 
+        yield return new WaitForSeconds(1f);
+
         rightIndicators.FlashIndicators((playerObj.transform.position.x >= 13.5) ? 9f : 5.5f, .25f, .125f); /// Gives players more time if they have fallen way to the left.
         yield return new WaitForSeconds((playerObj.transform.position.x >= 13.5) ? 9f : 5.5f);
-        Debug.Log("Macrowave from the RIGHT");
+        //Debug.Log("Macrowave from the RIGHT");
         rightRayHazards.EmitBegin();
         yield return new WaitForSeconds(5f);
         rightRayHazards.EmitEnd();
 
+        yield return new WaitForSeconds(1f);
+
         leftIndicators.FlashIndicators((playerObj.transform.position.x <= -11.5) ? 8f : 5f, .25f, .125f); /// Gives players more time if they fallen way to the left.
         yield return new WaitForSeconds((playerObj.transform.position.x <= -11.5) ? 8f : 5f);
-        Debug.Log("Macrowave from the LEFT");
+        //Debug.Log("Macrowave from the LEFT");
         leftRayHazards.EmitBegin();
         yield return new WaitForSeconds(5f);
         leftRayHazards.EmitEnd();
+
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("Should have set the parameter to be fucking 1.");
+        scrapAnimator.SetInteger("sequenceState", 1);
+
+        yield return new WaitForSeconds(3f);
+
+        leftIndicators.FlashIndicators(5f, .25f, .125f); 
+        rightIndicators.FlashIndicators(5f, .25f, .125f);
+        yield return new WaitForSeconds(5f);
+        //Debug.Log("Macrowave from the LEFT and RIGHT");
+        leftRayHazards.EmitBegin();
+        rightRayHazards.EmitBegin();
+        yield return new WaitForSeconds(5f);
+        leftRayHazards.EmitEnd();
+        rightRayHazards.EmitEnd();
+
+        yield return new WaitForSeconds(.5f);
+
+        topIndicators.FlashIndicators(7f, .25f, .125f);
+        rightIndicators.FlashIndicators(7f, .25f, .125f);
+        yield return new WaitForSeconds(7f);
+        //Debug.Log("Macrowave from the RIGHT and TOP");
+        topRayHazards.EmitBegin();
+        rightRayHazards.EmitBegin();
+        yield return new WaitForSeconds(5f);
+        topRayHazards.EmitEnd();
+        rightRayHazards.EmitEnd();
+
+        yield return new WaitForSeconds(.5f);
+
+        topIndicators.FlashIndicators(10f, .25f, .125f);
+        leftIndicators.FlashIndicators(10f, .25f, .125f);
+        yield return new WaitForSeconds(10f);
+        //Debug.Log("Macrowave from the LEFT and TOP");
+        topRayHazards.EmitBegin();
+        leftRayHazards.EmitBegin();
+        yield return new WaitForSeconds(5f);
+        topRayHazards.EmitEnd();
+        leftRayHazards.EmitEnd();
+
+        yield return new WaitForSeconds(.5f);
+        topIndicators.FlashIndicators(3f, .25f, .125f);
+        leftIndicators.FlashIndicators(3f, .25f, .125f);
+        leftIndicators.FlashIndicators(3f, .25f, .125f);
+        yield return new WaitForSeconds(3f);
+        // Macrowave breaks and shuts down.
+
+        // Disable barriers.
+        foreach (GameObject obj in barrierObjs)
+        {
+            obj.SetActive(false);
+        }
+
+        // Enable camera bounds in hallways.
+        foreach (Collider2D col in hallwayCamBounds)
+        {
+            col.gameObject.SetActive(true);
+            CameraManager.instance.UpdateConfinedBounds();
+        }
     }
 
 
