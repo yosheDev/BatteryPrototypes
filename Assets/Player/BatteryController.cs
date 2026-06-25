1d90a5836e28f77a07bf2a00837e261c6cef4777
Jacob Jones <101999077+yosheDev@users.noreply.github.com>
2026-06-24 20:10:18 -0500

    Removed bumpers control

--- Assets/Player/BatteryController.cs
+++ Assets/Player/BatteryController.cs
@@ -1438,138 +1438,138 @@ public class BatteryController : MonoBehaviour, IDamageable
 
     public void RightClick(InputAction.CallbackContext context)
     {
-        if (inputMode != PlayerInputMode.Enabled || GameInstance.instance.isGamePaused)
-        {
-            return;
-        }
+        //if (inputMode != PlayerInputMode.Enabled || GameInstance.instance.isGamePaused)
+        //{
+        //    return;
+        //}
 
-        if (context.started)
-        {
-            if (!isGrappling)
-            {
-                // Grapple has higher priority than bumpers. Bumpers will not happen if grapple does.
-                #region Grapple
-                List<Collider2D> grapplePoints = new List<Collider2D>();
-                Collider2D[] grappleCircleOverlap = Physics2D.OverlapCircleAll(transform.position, grappleRange, (int)LayerMask.GetMask("Default"));
-                foreach (Collider2D col in grappleCircleOverlap)
-                {
-                    if (col.CompareTag("Grapple"))
-                    {
-                        grapplePoints.Add(col);
-                    }
-                }
+        //if (context.started)
+        //{
+        //    if (!isGrappling)
+        //    {
+        //        // Grapple has higher priority than bumpers. Bumpers will not happen if grapple does.
+        //        #region Grapple
+        //        List<Collider2D> grapplePoints = new List<Collider2D>();
+        //        Collider2D[] grappleCircleOverlap = Physics2D.OverlapCircleAll(transform.position, grappleRange, (int)LayerMask.GetMask("Default"));
+        //        foreach (Collider2D col in grappleCircleOverlap)
+        //        {
+        //            if (col.CompareTag("Grapple"))
+        //            {
+        //                grapplePoints.Add(col);
+        //            }
+        //        }
 
-                if (grapplePoints.Count > 0)
-                {
-                    // Get the closest one.
-                    Collider2D nearestGrapplePoint = (grapplePoints.OrderBy(c => (transform.position - c.transform.position).sqrMagnitude).ToList())[0];
-                    grapplePoint = nearestGrapplePoint.transform;
+        //        if (grapplePoints.Count > 0)
+        //        {
+        //            // Get the closest one.
+        //            Collider2D nearestGrapplePoint = (grapplePoints.OrderBy(c => (transform.position - c.transform.position).sqrMagnitude).ToList())[0];
+        //            grapplePoint = nearestGrapplePoint.transform;
 
-                    // Update renderer
-                    Transform[] lineTrans = new Transform[2];
-                    lineTrans[0] = transform;
-                    lineTrans[1] = grapplePoint;
-                    grappleRenderer.SetPoints(lineTrans);
-                    grappleRenderer.SetState(true);
+        //            // Update renderer
+        //            Transform[] lineTrans = new Transform[2];
+        //            lineTrans[0] = transform;
+        //            lineTrans[1] = grapplePoint;
+        //            grappleRenderer.SetPoints(lineTrans);
+        //            grappleRenderer.SetState(true);
 
-                    // Update state
-                    grappleJoint.distance = Mathf.Clamp(1.05f * Vector2.Distance(transform.position, grapplePoint.position), 1.5f, float.MaxValue);
-                    grappleJoint.connectedAnchor = nearestGrapplePoint.transform.position;
-                    //grappleJoint.enabled = true;
+        //            // Update state
+        //            grappleJoint.distance = Mathf.Clamp(1.05f * Vector2.Distance(transform.position, grapplePoint.position), 1.5f, float.MaxValue);
+        //            grappleJoint.connectedAnchor = nearestGrapplePoint.transform.position;
+        //            //grappleJoint.enabled = true;
 
-                    isGrappling = true;
+        //            isGrappling = true;
 
-                    return;
-                }
-                #endregion
+        //            return;
+        //        }
+        //        #endregion
 
-                #region Bumpers
-                Collider2D[] positiveEndOverlap = Physics2D.OverlapCircleAll((Vector2)positiveMag.transform.position + (Vector2)(positiveMag.transform.up * .2f), .5f, 1 << LayerMask.NameToLayer("Default"));
-                Collider2D[] negativeEndOverlap = Physics2D.OverlapCircleAll((Vector2)negativeMag.transform.position + (Vector2)(negativeMag.transform.up * .2f), .5f, 1 << LayerMask.NameToLayer("Default"));
+        //        #region Bumpers
+        //        Collider2D[] positiveEndOverlap = Physics2D.OverlapCircleAll((Vector2)positiveMag.transform.position + (Vector2)(positiveMag.transform.up * .2f), .5f, 1 << LayerMask.NameToLayer("Default"));
+        //        Collider2D[] negativeEndOverlap = Physics2D.OverlapCircleAll((Vector2)negativeMag.transform.position + (Vector2)(negativeMag.transform.up * .2f), .5f, 1 << LayerMask.NameToLayer("Default"));
 
-                int positiveOverlaps = 0;
-                int negativeOverlaps = 0;
+        //        int positiveOverlaps = 0;
+        //        int negativeOverlaps = 0;
 
-                #region Filter Results
-                for (int i = 0; i < positiveEndOverlap.Length; i++)
-                {
-                    if (!positiveEndOverlap[i].gameObject.CompareTag("Player"))
-                    {
-                        positiveOverlaps++;
-                    }
-                    else
-                    {
-                        positiveEndOverlap[i] = null;
-                    }
-                }
+        //        #region Filter Results
+        //        for (int i = 0; i < positiveEndOverlap.Length; i++)
+        //        {
+        //            if (!positiveEndOverlap[i].gameObject.CompareTag("Player"))
+        //            {
+        //                positiveOverlaps++;
+        //            }
+        //            else
+        //            {
+        //                positiveEndOverlap[i] = null;
+        //            }
+        //        }
 
-                for (int i = 0; i < negativeEndOverlap.Length; i++)
-                {
-                    if (!negativeEndOverlap[i].gameObject.CompareTag("Player"))
-                    {
-                        negativeOverlaps++;
-                    }
-                    else
-                    {
-                        negativeEndOverlap[i] = null;
-                    }
-                }
-                #endregion
+        //        for (int i = 0; i < negativeEndOverlap.Length; i++)
+        //        {
+        //            if (!negativeEndOverlap[i].gameObject.CompareTag("Player"))
+        //            {
+        //                negativeOverlaps++;
+        //            }
+        //            else
+        //            {
+        //                negativeEndOverlap[i] = null;
+        //            }
+        //        }
+        //        #endregion
 
-                //Debug.Log(positiveOverlaps + negativeOverlaps);
-                if (positiveOverlaps + negativeOverlaps > 0)
-                {
-                    if (positiveOverlaps > 0)
-                    {
-                        Vector2 dir = Vector2.zero;
-                        Vector2[] dirs = new Vector2[positiveEndOverlap.Length];
-                        for (int i = 0; i < positiveEndOverlap.Length; i++)
-                        {
-                            if (positiveEndOverlap[i] == null)
-                            {
-                                continue;
-                            }
+        //        //Debug.Log(positiveOverlaps + negativeOverlaps);
+        //        if (positiveOverlaps + negativeOverlaps > 0)
+        //        {
+        //            if (positiveOverlaps > 0)
+        //            {
+        //                Vector2 dir = Vector2.zero;
+        //                Vector2[] dirs = new Vector2[positiveEndOverlap.Length];
+        //                for (int i = 0; i < positiveEndOverlap.Length; i++)
+        //                {
+        //                    if (positiveEndOverlap[i] == null)
+        //                    {
+        //                        continue;
+        //                    }
 
-                            dirs[i] = ((Vector2)positiveMag.transform.position - positiveEndOverlap[i].ClosestPoint((Vector2)positiveMag.transform.position)).normalized;
-                            dir += dirs[i];
-                        }
-                        dir /= dirs.Length;
-                        rb.AddForceAtPosition(Vector2.Lerp(dir, -positiveMag.transform.up, 0.5f) * 700f, positiveMag.transform.position);
-                    }
+        //                    dirs[i] = ((Vector2)positiveMag.transform.position - positiveEndOverlap[i].ClosestPoint((Vector2)positiveMag.transform.position)).normalized;
+        //                    dir += dirs[i];
+        //                }
+        //                dir /= dirs.Length;
+        //                rb.AddForceAtPosition(Vector2.Lerp(dir, -positiveMag.transform.up, 0.5f) * 700f, positiveMag.transform.position);
+        //            }
 
-                    if (negativeOverlaps > 0)
-                    {
-                        Vector2 dir = Vector2.zero;
-                        Vector2[] dirs = new Vector2[negativeEndOverlap.Length];
-                        for (int i = 0; i < negativeEndOverlap.Length; i++)
-                        {
-                            if (negativeEndOverlap[i] == null)
-                            {
-                                continue;
-                            }
-                            //Debug.Log(negativeEndOverlap[i]);
-                            dirs[i] = ((Vector2)negativeMag.transform.position - negativeEndOverlap[i].ClosestPoint((Vector2)negativeMag.transform.position)).normalized;
-                            //Debug.DrawLine(negativeMag.transform.position, negativeMag.transform.position + ((Vector3)dirs[i] * 5f), Color.green, 1f);
-                            dir += dirs[i];
-                        }
-                        dir /= dirs.Length;
-                        rb.AddForceAtPosition(Vector2.Lerp(dir, -negativeMag.transform.up, 0.5f) * 700f, negativeMag.transform.position);
-                        //Debug.DrawLine(negativeMag.transform.position, negativeMag.transform.position + ((Vector3)dir * 5f), Color.red, 1f);
-                    }
-                }
-                #endregion
-            }
+        //            if (negativeOverlaps > 0)
+        //            {
+        //                Vector2 dir = Vector2.zero;
+        //                Vector2[] dirs = new Vector2[negativeEndOverlap.Length];
+        //                for (int i = 0; i < negativeEndOverlap.Length; i++)
+        //                {
+        //                    if (negativeEndOverlap[i] == null)
+        //                    {
+        //                        continue;
+        //                    }
+        //                    //Debug.Log(negativeEndOverlap[i]);
+        //                    dirs[i] = ((Vector2)negativeMag.transform.position - negativeEndOverlap[i].ClosestPoint((Vector2)negativeMag.transform.position)).normalized;
+        //                    //Debug.DrawLine(negativeMag.transform.position, negativeMag.transform.position + ((Vector3)dirs[i] * 5f), Color.green, 1f);
+        //                    dir += dirs[i];
+        //                }
+        //                dir /= dirs.Length;
+        //                rb.AddForceAtPosition(Vector2.Lerp(dir, -negativeMag.transform.up, 0.5f) * 700f, negativeMag.transform.position);
+        //                //Debug.DrawLine(negativeMag.transform.position, negativeMag.transform.position + ((Vector3)dir * 5f), Color.red, 1f);
+        //            }
+        //        }
+        //        #endregion
+        //    }
 
-        }
-        else if (context.canceled)
-        {
-            if (isGrappling)
-            {
-                grappleRenderer.SetState(false);
-                grappleJoint.enabled = false;
-                isGrappling = false;
-            }
-        }
+        //}
+        //else if (context.canceled)
+        //{
+        //    if (isGrappling)
+        //    {
+        //        grappleRenderer.SetState(false);
+        //        grappleJoint.enabled = false;
+        //        isGrappling = false;
+        //    }
+        //}
     }
     #endregion
     public void ChangeWeldState(WeldState newState)
